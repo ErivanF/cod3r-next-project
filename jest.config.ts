41fef45ecf9592beeff96a341a0dccd0ca335d7d
ErivanF <erivan.c39@gmail.com>
2024-10-27 18:48:31 -0300

    test: added cypress lib

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,10 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
+import nextJest from "next/jest";
 
+const createJestConfig = nextJest({ dir: "./" });
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -196,4 +198,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
