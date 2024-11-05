d71e0c0a1ee826fdd336b1eb06a4601554eecae7
ErivanF <erivan.c39@gmail.com>
2024-11-05 07:52:33 -0300

    chore: removed Input from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
 "use client";
-import Input from "@/components/Input";
-import { useState } from "react";
 
 export default function Home() {
-  const [value, setValue] = useState("");
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Input
-        title="nome"
-        id="name"
-        value={value}
-        handleChange={(newValue: string) => {
-          setValue(newValue);
-        }}
-      />
-    </div>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
   );
 }
