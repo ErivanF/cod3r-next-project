79d0d70b3f5a4bcacd8088726a76bbb4c44abca0
ErivanF <erivan.c39@gmail.com>
2024-11-05 15:53:57 -0300

    feat: created table header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import TableHeader from "@/components/TableHeader";
+
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <TableHeader />
+    </div>
   );
 }
--- src/components/TableHeader/index.tsx
+++ src/components/TableHeader/index.tsx
@@ -1,1 +1,18 @@
-export default function TableHeader() {}
+export default function TableHeader() {
+  return (
+    <div className="flex justify-stretch w-full">
+      <div className="bg-violet-900 border-x-white grow text-center p-1 border-r-2 rounded-tl-md">
+        Idade
+      </div>
+      <div className="bg-violet-900 border-x-white grow text-center p-1 border-r-2">
+        Nome
+      </div>
+      <div className="bg-violet-900 border-x-white grow text-center p-1 border-r-2">
+        Ações
+      </div>
+      <div className="bg-violet-900 border-x-white grow text-center p-1 rounded-tr-md ">
+        ID
+      </div>
+    </div>
+  );
+}
