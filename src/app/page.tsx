011c66dab62226f38d8d3cca625678dfc41e46ab
ErivanF <erivan.c39@gmail.com>
2024-11-05 07:51:49 -0300

    feature: input funcionality and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Input from "@/components/Input";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Input />
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Input
+        title="nome"
+        id="name"
+        value={value}
+        handleChange={(newValue: string) => {
+          setValue(newValue);
+        }}
+      />
     </div>
   );
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,30 @@
-export default function Input() {
-  return <></>;
+interface props {
+  title: string;
+  id: string;
+  value: string;
+  handleChange: (value: string) => void;
+  type?: "text" | "number";
+}
+export default function Input({
+  id,
+  title,
+  type = "text",
+  value,
+  handleChange,
+}: props) {
+  return (
+    <div className="p-1 flex flex-col max-w-md h-11">
+      <div className="text-gray-200 text-lg m-1">{title}</div>
+
+      <input
+        className=" text-black p-2 border-s-black rounded"
+        data-testid={id}
+        type={type}
+        value={value}
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
