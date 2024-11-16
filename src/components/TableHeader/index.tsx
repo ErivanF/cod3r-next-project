a45957884066dcf964b97a6ddfe071fbdd9bdd01
ErivanF <erivan.c39@gmail.com>
2024-11-16 09:15:42 -0300

    fix: fixed console errors

--- src/components/TableHeader/index.tsx
+++ src/components/TableHeader/index.tsx
@@ -2,18 +2,18 @@ export default function TableHeader() {
   return (
     <thead className="flex flex-col">
       <tr className="flex justify-stretch w-full">
-        <div className="bg-violet-900 border-x-white basis-1 grow text-center p-1 border-r-2 rounded-tl-md">
+        <td className="bg-violet-900 border-x-white basis-1 grow text-center p-1 border-r-2 rounded-tl-md">
           ID
-        </div>
-        <div className="bg-violet-900 border-x-white basis-1 grow text-center p-1 border-r-2">
+        </td>
+        <td className="bg-violet-900 border-x-white basis-1 grow text-center p-1 border-r-2">
           Nome
-        </div>
-        <div className="bg-violet-900 border-x-white basis-1 grow text-center p-1 border-r-2">
+        </td>
+        <td className="bg-violet-900 border-x-white basis-1 grow text-center p-1 border-r-2">
           Idade
-        </div>
-        <div className="bg-violet-900 border-x-white basis-1 grow text-center p-1 rounded-tr-md ">
+        </td>
+        <td className="bg-violet-900 border-x-white basis-1 grow text-center p-1 rounded-tr-md ">
           Ações
-        </div>
+        </td>
       </tr>
     </thead>
   );
