bdf990969644c4e6f13145c91ec37b09f58e5663
ErivanF <erivan.c39@gmail.com>
2024-11-16 09:16:57 -0300

    chore:removed elements from main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import TableHeader from "@/components/TableHeader";
-import TableLine from "@/components/TableLine";
-import User from "@/Core/UserModel";
-const testUser = new User("User", 12345, "1q2w3e4r");
 export default function Home() {
-  return (
-    <table className="min-h-screen flex flex-col p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <TableHeader />
-      <TableLine user={testUser} />
-    </table>
-  );
+  return <></>;
 }
