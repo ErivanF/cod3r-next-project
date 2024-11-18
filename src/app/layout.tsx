e5ec1b1247a264305fc155511ec2683119f06627
ErivanF <erivan.c39@gmail.com>
2024-11-18 15:42:04 -0300

    feat: layout added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-black min-h-screen text-white 2xl:text-lg">
+          <div className="bg-purple-950 flex items-center text-center text-white font-bold text-xl pl-4 sm:text-4xl ">
+            <Link href="/form" className="border-x-2 p-4">
+              Criar usuário
+            </Link>
+            <Link href="/" className="border-x-2 p-4">
+              Lista
+            </Link>
+          </div>
+          <div className="p-12 flex flex-col items-center"> {children}</div>
+        </div>
       </body>
     </html>
   );
