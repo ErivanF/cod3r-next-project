76322785be27ab1101a8eb6b133e120c98f78c76
ErivanF <erivan.c39@gmail.com>
2024-11-18 11:50:51 -0300

    feat: added list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import TableHeader from "@/components/TableHeader";
+import TableLine from "@/components/TableLine";
+import User from "@/Core/UserModel";
+import UserCollection from "@/db/UserCollection";
+import { useEffect, useState } from "react";
+const repo = new UserCollection();
 export default function Home() {
-  return <></>;
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    repo.getAll().then((data) => {
+      setUsers(data);
+    });
+  }, []);
+  return (
+    <table className="w-full">
+      <TableHeader />
+      {users.map((user) => (
+        <TableLine key={user.id} user={user} />
+      ))}
+    </table>
+  );
 }
