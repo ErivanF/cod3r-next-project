00df592c7ccf3f9543b07f17de40e858d2f81465
ErivanF <erivan.c39@gmail.com>
2024-12-13 17:14:10 -0300

    fix: fixed unit tests

--- src/components/TableLine/__tests__/index.tsx
+++ src/components/TableLine/__tests__/index.tsx
@@ -2,28 +2,49 @@ import { render, screen } from "@testing-library/react";
 import TableLine from "..";
 
 import User from "@/Core/UserModel";
+import { AppRouterContextProviderMock } from "@/utils/RouterProviderMock";
 
 const testUser = new User("testUserName", 9876, "userID");
 
 describe("TableLine", () => {
   it("should display the user ID", () => {
-    render(<TableLine user={testUser} />);
+    render(
+      <AppRouterContextProviderMock router={{}}>
+        <TableLine user={testUser} />
+      </AppRouterContextProviderMock>
+    );
     expect(screen.getByText(testUser.id.toString())).toBeInTheDocument();
   });
   it("should display the user name", () => {
-    render(<TableLine user={testUser} />);
+    render(
+      <AppRouterContextProviderMock router={{}}>
+        <TableLine user={testUser} />
+      </AppRouterContextProviderMock>
+    );
     expect(screen.getByText(testUser.name)).toBeInTheDocument();
   });
   it("should display the user age", () => {
-    render(<TableLine user={testUser} />);
+    render(
+      <AppRouterContextProviderMock router={{}}>
+        <TableLine user={testUser} />
+      </AppRouterContextProviderMock>
+    );
     expect(screen.getByText(testUser.age.toString())).toBeInTheDocument();
   });
   it("should have an edit button", () => {
-    render(<TableLine user={testUser} />);
+    render(
+      <AppRouterContextProviderMock router={{}}>
+        <TableLine user={testUser} />
+      </AppRouterContextProviderMock>
+    );
     expect(screen.getByTestId("editButton")).toBeInTheDocument();
   });
   it("should have a delete button", () => {
-    render(<TableLine user={testUser} />);
+    render(
+      <AppRouterContextProviderMock router={{}}>
+        <TableLine user={testUser} />
+      </AppRouterContextProviderMock>
+    );
     expect(screen.getByTestId("deleteButton")).toBeInTheDocument();
   });
 });
